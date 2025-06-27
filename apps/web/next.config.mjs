8f39104625579a3f3c86496665a8175a950c6b5c
trungtran <baotrung06092003@gmail.com>
2025-06-27 23:11:26 +0700

    fix: config image url

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@workspace/ui"],
-}
+  transpilePackages: ['@workspace/ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
